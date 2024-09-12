ec971fe722305aa85322c2a7533b57ebe00f011e
rafasapiens <rafaelreiscl@gmail.com>
2024-09-12 06:05:24 -0300

    adicionados webscraping julho agosto

---
+++ obituary/obituario_agosto2024_pandas.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+tabela = pd.read_csv("obituario_agosto2024.csv", sep=",")
+
+print(tabela)
+
+#display(tabela)
+# To display the top 5 rows
+tabela.head(5)
+
+# To display the botton 5 rows
+print(tabela.tail(60))
+
+# To display types of data
+print(tabela.dtypes)
+
+
