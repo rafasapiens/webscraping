b84740fdfbb9b1d6702966337c9fa2e57aadd2b2
rafasapiens <rafaelreiscl@gmail.com>
2025-08-07 18:35:04 -0300

    obitos julho2025

---
+++ obituary/obituario_julho2025_pandas.py
@@ -0,0 +1,21 @@
+import pandas as pd
+
+tabela = pd.read_csv("obituario_julho2025.csv", sep=",")
+print(tabela)
+
+#display(tabela)
+# To display the top 5 rows
+tabela.head(5)
+
+# To display the botton 5 rows
+print(tabela.tail(60))
+
+# To display types of data
+print(tabela.dtypes)
+
+# To describe data
+print("Describe Data: \n",tabela.describe)
+
+# To info data
+print("Info: \n",tabela.info)
+
